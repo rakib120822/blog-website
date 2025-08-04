26d495d59bf3d5b121ade32b283f325d6a9c0be6
rakib120822 <payelrakibulislam@gmail.com>
2025-08-04 14:48:12 +0600

    add search bar in navber

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -11,7 +11,18 @@ function Navbar() {
           INSIGHTS
         </h1>
 
-        <div className="flex  gap-22 text-[1rem] leading-[120%] space-[3%] font-Montserrat py-[40px] text-rich-black-50">
+        <div class="relative w-[40%]">
+          <input
+            type="text"
+            placeholder="Search by topic, tag, category, or resource..."
+            class=" pl-3  py-2 w-full border rounded-[32px] border-rich-black-300 placeholder:text-rich-black-100"
+          />
+          <span class="absolute inset-y-0 right-0 flex items-center pr-2 text-rich-black-50">
+            <i class="fas fa-search"></i>
+          </span>
+        </div>
+
+        <div className="flex  gap-10 text-[1rem] leading-[120%] space-[3%] font-Montserrat py-[40px] text-rich-black-50">
           <NavLink to="/">Home</NavLink>
           <NavLink to="blogs">Blog</NavLink>
           <NavLink to="blogs">Resources</NavLink>
